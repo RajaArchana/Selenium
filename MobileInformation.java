f3a01af397e9a0b3c59b14734a2644bb7e8a5aae
RajaArchana <88586271+RajaArchana@users.noreply.github.com>
2021-08-08 09:16:10 +0530

    Add files via upload

---
+++ MobileInformation.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class MobileInformation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String brandName = "OnePlus";
+String model = "Pro9";
+char color = 'W';
+int price = 40000;
+byte memory =120;
+float screenSize = 14.5f;
+boolean isTouchScreen = true;
+long phnNumber = 9600844879L;
+System.out.println("My phone brand is "+brandName+" ,model is "+model+
+		" ,color is "+color+",price is"+price+", memory is "+memory+
+		", the screen size is "+screenSize+" and my phone number is"+phnNumber);
+	}
+
+}
